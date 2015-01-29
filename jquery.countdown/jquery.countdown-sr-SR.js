fcc84a7ddf6543a37766441a7863bc4ffb623c6b
Brian R. Jackson <brian@jaxzin.com>
2015-01-28 22:14:22 -0500

    Fixed countdown timer. Added support for counting up if the watershed moment has since passed.

--- jquery.countdown/jquery.countdown-sr-SR.js
+++ jquery.countdown/jquery.countdown-sr-SR.js
@@ -1,8 +1,8 @@
-﻿/* http://keith-wood.name/countdown.html
+/* http://keith-wood.name/countdown.html
  * Serbian Latin initialisation for the jQuery countdown extension
  * Written by Predrag Leka lp@lemurcake.com (2010) */
 (function($) {
-	$.countdown.regional['sr-SR'] = {
+	$.countdown.regionalOptions['sr-SR'] = {
 		labels: ['Godina', 'Meseci', 'Nedelja', 'Dana', 'Časova', 'Minuta', 'Sekundi'],
 		labels1: ['Godina', 'Mesec', 'Nedelja', 'Dan', 'Čas', 'Minut', 'Sekunda'],
 		labels2: ['Godine', 'Meseca', 'Nedelje', 'Dana', 'Časa', 'Minuta', 'Sekunde'],
@@ -10,6 +10,7 @@
 		whichLabels: function(amount) {
 			return (amount == 1 ? 1 : (amount >= 2 && amount <= 4 ? 2 : 0));
 		},
+		digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
 		timeSeparator: ':', isRTL: false};
-	$.countdown.setDefaults($.countdown.regional['sr-SR']);
+	$.countdown.setDefaults($.countdown.regionalOptions['sr-SR']);
 })(jQuery);
